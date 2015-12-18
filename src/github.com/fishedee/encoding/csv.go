39cbd673d5b5817f91070b81458fb6f5fb4ab075
fishedee <306766045@qq.com>
2015-12-18 22:12:10 +0800

    加入csv的utf-8头bom

--- src/github.com/fishedee/encoding/csv.go
+++ src/github.com/fishedee/encoding/csv.go
@@ -8,6 +8,7 @@ import (
 func EncodeCsv(data [][]string)([]byte,error){
 	var writer bytes.Buffer
 	csvWriter := csv.NewWriter(&writer)
+	writer.Write([]byte("\xEF\xBB\xBF"))
 	for i := 0 ; i != len(data) ; i++{
 		err := csvWriter.Write(data[i])
 		if err != nil{
