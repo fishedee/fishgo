22a355f88286c7f0b8c11d0648e12fb942e122fe
jd <donnol@outlook.com>
2015-12-18 22:30:13 +0800

    修改获取图片尺寸信息接口

--- src/github.com/fishedee/util/image.go
+++ src/github.com/fishedee/util/image.go
@@ -2,18 +2,56 @@ package util
 
 import (
 	"bytes"
+	"errors"
 	"image"
 	_ "image/gif"
 	_ "image/jpeg"
 	_ "image/png"
+	"os"
 )
 
 type ImageSize struct {
 	Width, Height int
 }
 
-func GetImageSize(data []byte) (ImageSize, error) {
-	jpegReader := bytes.NewReader(data)
+type Image struct {
+	data []byte
+}
+
+func NewImageFromString(data []byte) (Image, error) {
+	return Image{
+		data: data,
+	}, nil
+}
+
+func NewImageFromFile(fileName string) (Image, error) {
+	file, err := os.Open(fileName)
+	if err != nil {
+		return Image{}, err
+	}
+	defer file.Close()
+	fileInfo, err := file.Stat()
+	if err != nil {
+		return Image{}, err
+	}
+
+	fileSize := int(fileInfo.Size())
+	data := make([]byte, fileSize)
+	size, err := file.Read(data)
+	if err != nil {
+		return Image{}, err
+	}
+	if size != fileSize {
+		return Image{}, errors.New("文件未读写完全！")
+	}
+
+	return Image{
+		data: data,
+	}, nil
+}
+
+func (this *Image) GetSize() (ImageSize, error) {
+	jpegReader := bytes.NewReader(this.data)
 	config, _, err := image.DecodeConfig(jpegReader)
 	if err != nil {
 		return ImageSize{}, err
