d0d9c26763091302bb202998fd8208f61c6e524c
jd <donnol@outlook.com>
2016-03-09 20:33:28 +0800

    修复byte数组的问题

--- src/github.com/fishedee/sdk/smtpsdk.go
+++ src/github.com/fishedee/sdk/smtpsdk.go
@@ -1,6 +1,7 @@
 package sdk
 
 import (
+	"bytes"
 	"encoding/base64"
 	"net/smtp"
 	"strings"
@@ -23,13 +24,22 @@ type SmtpSdkMailAttach struct {
 	Data []byte
 }
 
-func (this *SmtpSdk) Auth(username, password string) smtp.Auth {
+type SmtpSdkMailAuth struct {
+	UserName string
+	Password string
+}
+
+func (this *SmtpSdk) auth(username, password string) smtp.Auth {
 	identity := ""
 	host := strings.Split(this.Host, ":")[0]
 	return smtp.PlainAuth(identity, username, password, host)
 }
 
-func (this *SmtpSdk) Send(auth smtp.Auth, mail SmtpSdkMail) error {
+func (this *SmtpSdk) Send(mailAuth SmtpSdkMailAuth, mail SmtpSdkMail) error {
+	auth := this.auth(mailAuth.UserName, mailAuth.Password)
+
+	msg := bytes.Buffer{}
+
 	//boundary
 	boundary := "XX-JDED00099ASCI"
 	assignBoundary := "\"" + boundary + "\""
@@ -43,25 +53,46 @@ func (this *SmtpSdk) Send(auth smtp.Auth, mail SmtpSdkMail) error {
 		toString += value + trimSign
 	}
 	toString = strings.Trim(toString, trimSign)
-	msg := []byte("To: " + toString + "\r\nFrom: " + mail.From + "<" + mail.From + ">\r\nSubject: " + mail.Subject + "\r\n" + "Content-Type: multipart/mixed; boundary=" + assignBoundary + "\r\n\r\n")
+	_, err := msg.WriteString("To: " + toString + "\r\nFrom: " + mail.From + "<" + mail.From + ">\r\nSubject: " + mail.Subject + "\r\n" + "Content-Type: multipart/mixed; boundary=" + assignBoundary + "\r\n\r\n")
+	if err != nil {
+		return err
+	}
 
 	//body
-	msg = append(msg, []byte(middleBoundary+"\r\n"+"Content-Type:text/plain; charset=UTF-8\r\n\r\n")...)
-	msg = append(msg, []byte(mail.Body+"\r\n\r\n")...)
+	_, err = msg.WriteString(middleBoundary + "\r\n" + "Content-Type:text/plain; charset=UTF-8\r\n\r\n")
+	if err != nil {
+		return err
+	}
+	_, err = msg.WriteString(mail.Body + "\r\n\r\n")
+	if err != nil {
+		return err
+	}
 
 	//attach
 	for _, value := range mail.Attach {
-		msg = append(msg, []byte(middleBoundary+"\r\n"+"Content-Type: application/octet-stream; charset=UTF-8\r\nContent-Disposition: attachment;filename="+value.Name+"\r\nContent-Description: File Transfer"+"\r\nContent-Transfer-Encoding: base64\r\n\r\n")...)
+		_, err = msg.WriteString(middleBoundary + "\r\n" + "Content-Type: application/octet-stream; charset=UTF-8\r\nContent-Disposition: attachment;filename=" + value.Name + "\r\nContent-Description: File Transfer" + "\r\nContent-Transfer-Encoding: base64\r\n\r\n")
+		if err != nil {
+			return err
+		}
 		afterData := this.base64Encode(value.Data)
-		msg = append(msg, afterData...)
-		msg = append(msg, []byte("\r\n\r\n")...)
+		_, err = msg.Write(afterData)
+		if err != nil {
+			return err
+		}
+		_, err = msg.WriteString("\r\n\r\n")
+		if err != nil {
+			return err
+		}
 	}
 
 	//结束标志
-	msg = append(msg, []byte(endBoundary+"\r\n")...)
+	_, err = msg.WriteString(endBoundary + "\r\n")
+	if err != nil {
+		return err
+	}
 
 	//send message
-	return smtp.SendMail(this.Host, auth, mail.From, mail.To, msg)
+	return smtp.SendMail(this.Host, auth, mail.From, mail.To, msg.Bytes())
 }
 
 func (this *SmtpSdk) base64Encode(data []byte) []byte {
