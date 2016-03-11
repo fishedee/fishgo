825373142294b86315c73dc40819463c2cc04bde
fishedee <306766045@qq.com>
2016-03-11 16:19:23 +0800

    新增zip压缩功能

---
+++ src/github.com/fishedee/encoding/zip.go
@@ -0,0 +1,63 @@
+package encoding
+
+import (
+	"archive/zip"
+	"errors"
+	"io"
+	"os"
+)
+
+func EncodeZipFile(inputFileName string, outputFileName string) error {
+	//建立读取文件
+	inputFile, err := os.Open(inputFileName)
+	if err != nil {
+		return err
+	}
+	defer inputFile.Close()
+	//建立写入文件
+	outputFile, err := os.Create(outputFileName)
+	if err != nil {
+		return err
+	}
+	defer outputFile.Close()
+	//zip压缩数据
+	zipWriter := zip.NewWriter(outputFile)
+	defer zipWriter.Close()
+	zipFileWriter, err := zipWriter.Create("data")
+	if err != nil {
+		return err
+	}
+	_, err = io.Copy(zipFileWriter, inputFile)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func DecodeZipFile(inputFileName string, outputFileName string) error {
+	//建立写入文件
+	outputFile, err := os.Create(outputFileName)
+	if err != nil {
+		return err
+	}
+	defer outputFile.Close()
+	//zip解压数据
+	zipReader, err := zip.OpenReader(inputFileName)
+	if err != nil {
+		return err
+	}
+	defer zipReader.Close()
+	if len(zipReader.File) == 0 {
+		return errors.New("zip is empty!")
+	}
+	fileOpen, err := zipReader.File[0].Open()
+	if err != nil {
+		return err
+	}
+	defer fileOpen.Close()
+	_, err = io.Copy(outputFile, fileOpen)
+	if err != nil {
+		return err
+	}
+	return nil
+}
---
+++ src/github.com/fishedee/encoding/zip_test.go
@@ -0,0 +1,55 @@
+package encoding
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+func ClearTestFile(file []string) {
+	for _, singleFile := range file {
+		os.Remove(singleFile)
+	}
+}
+
+func TestZipNormal(t *testing.T) {
+	testCase := []string{
+		"",
+		"123",
+		"你好",
+	}
+	txtFile := "zipTest.txt"
+	zipFile := "zipTest.zip"
+	txtFile2 := "zipTest2.txt"
+	defer ClearTestFile([]string{txtFile, zipFile, txtFile2})
+	for _, singleTestCase := range testCase {
+		var err error
+		//准备数据
+		ClearTestFile([]string{txtFile, zipFile, txtFile2})
+		err = ioutil.WriteFile(txtFile, []byte(singleTestCase), os.ModePerm)
+		if err != nil {
+			panic(err)
+		}
+		//压缩
+		err = EncodeZipFile(txtFile, zipFile)
+		if err != nil {
+			t.Error("err is not nil! " + err.Error())
+			return
+		}
+		err = DecodeZipFile(zipFile, txtFile2)
+		if err != nil {
+			t.Error("err is not nil! " + err.Error())
+			return
+		}
+		result, err := ioutil.ReadFile(txtFile2)
+		if err != nil {
+			t.Error("err is not nil! " + err.Error())
+			return
+		}
+		resultString := string(result)
+		if resultString != singleTestCase {
+			t.Errorf("%s != %s", resultString, singleTestCase)
+			return
+		}
+	}
+}
