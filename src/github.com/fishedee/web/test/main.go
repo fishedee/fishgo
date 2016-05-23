d97eb3183125b011a8481572515696b0bcaa120c
fishedee <306766045@qq.com>
2016-05-23 15:41:13 +0800

    加入router的性能测试代码

--- src/github.com/fishedee/web/test/main.go
+++ src/github.com/fishedee/web/test/main.go
@@ -1,10 +1,23 @@
 package main
 
 import (
-	"github.com/astaxie/beego"
+	"github.com/fishedee/web"
 )
+
+type testController struct {
+	web.Controller
+}
+
+func (this *testController) Doing_Json() interface{} {
+	return "Hello World"
+}
+
+func (this *testController) AutoRender(data interface{}, viewname string) {
+	this.Ctx.Write([]byte(data.(string)))
+}
 
 //go:generate fishgen ^./models/.*(ao|db)\.go$
 func main() {
-	beego.Run()
+	web.InitRoute("", &testController{})
+	web.Run()
 }
