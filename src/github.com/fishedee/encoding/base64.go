090998d0584ae410ee3dbef0d746b770fa1908ac
jd <donnol@outlook.com>
2017-02-15 10:23:55 +0800

    添加安全url编码方法

--- src/github.com/fishedee/encoding/base64.go
+++ src/github.com/fishedee/encoding/base64.go
@@ -2,6 +2,7 @@ package encoding
 
 import (
 	"encoding/base64"
+	"strings"
 )
 
 func EncodeBase64(in []byte) (string, error) {
@@ -14,4 +15,12 @@ func DecodeBase64(in string) ([]byte, error) {
 		return nil, err
 	}
 	return result, nil
+}
+
+// url安全的base64编码
+func EncodeBase64Safe(in []byte) (string, error) {
+	encData := base64.StdEncoding.EncodeToString(in)
+	encData = strings.Replace(encData, `+`, `-`, -1)
+	encData = strings.Replace(encData, `/`, `_`, -1)
+	return encData, nil
 }
