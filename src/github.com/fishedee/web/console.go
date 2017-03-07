e7083783b68b5da490e4f2f10f18438364a9958e
fishedee <306766045@qq.com>
2017-03-07 20:23:44 +0800

    加入嵌入web的方法

--- src/github.com/fishedee/web/console.go
+++ src/github.com/fishedee/web/console.go
@@ -1,27 +1,15 @@
 package web
 
 import (
-	"errors"
 	"reflect"
 )
 
-func RunMain(handler interface{}) error {
-	handlerValue := reflect.ValueOf(handler)
-	handlerType := reflect.TypeOf(handler)
-	if handlerType.Kind() != reflect.Func {
-		return errors.New("invalid handler not function")
-	}
-	if handlerType.NumIn() != 1 {
-		return errors.New("invalid handler should has a parameter")
-	}
-	handlerFirstArgv := handlerType.In(0)
-	if handlerFirstArgv.Kind() != reflect.Ptr {
-		return errors.New("invalid handler first parameter is not a ptr")
-	}
-	handlerFirstArgv = handlerFirstArgv.Elem()
-	target := reflect.New(handlerFirstArgv)
+func InitModel(handler interface{}) {
+	target := reflect.ValueOf(handler)
 	basic := initEmptyBasic(nil)
 	injectIoc(target, basic)
-	handlerValue.Call([]reflect.Value{target})
-	return nil
+}
+
+func InitController(handler interface{}){
+	InitModel(handler)
 }
