8c874c5f48172da4cfdf66c967b9690513f67662
fishedee <306766045@qq.com>
2018-01-24 07:56:12 +0800

    重写smtp实现，支持ssl和修复附件文件名乱码

--- src/github.com/fishedee/sdk/smtpsdk.go
+++ src/github.com/fishedee/sdk/smtpsdk.go
@@ -2,13 +2,19 @@ package sdk
 
 import (
 	"bytes"
-	"encoding/base64"
-	"net/smtp"
-	"strings"
+	"crypto/tls"
+	"errors"
+	"github.com/fishedee/encoding"
+	"github.com/fishedee/language"
+	"gopkg.in/gomail.v2"
+	"io"
+	"strconv"
 )
 
 type SmtpSdk struct {
-	Host string
+	Host      string
+	SSL       bool
+	TLSConfig *tls.Config
 }
 
 type SmtpSdkMail struct {
@@ -29,76 +35,41 @@ type SmtpSdkMailAuth struct {
 	Password string
 }
 
-func (this *SmtpSdk) auth(username, password string) smtp.Auth {
-	identity := ""
-	host := strings.Split(this.Host, ":")[0]
-	return smtp.PlainAuth(identity, username, password, host)
+func (this *SmtpSdk) setAttach(m *gomail.Message, singleAttach SmtpSdkMailAttach) {
+	base64name, _ := encoding.EncodeBase64([]byte(singleAttach.Name))
+	base64nameString := "=?utf-8?B?" + string(base64name) + "?="
+	m.Attach(base64nameString, gomail.SetCopyFunc(func(dest io.Writer) error {
+		src := bytes.NewReader(singleAttach.Data)
+		_, err := io.Copy(dest, src)
+		return err
+	}))
 }
-
 func (this *SmtpSdk) Send(mailAuth SmtpSdkMailAuth, mail SmtpSdkMail) error {
-	auth := this.auth(mailAuth.UserName, mailAuth.Password)
-
-	msg := bytes.Buffer{}
-
-	//boundary
-	boundary := "XX-JDED00099ASCI"
-	assignBoundary := "\"" + boundary + "\""
-	middleBoundary := "--" + boundary
-	endBoundary := middleBoundary + "--"
-
-	//header
-	toString := ""
-	trimSign := ","
-	for _, value := range mail.To {
-		toString += value + trimSign
+	m := gomail.NewMessage()
+	m.SetHeader("From", mail.From)
+	m.SetHeader("To", mail.To...)
+	m.SetHeader("Subject", mail.Subject)
+	m.SetBody("text/html", mail.Body)
+	for _, singleAttach := range mail.Attach {
+		this.setAttach(m, singleAttach)
 	}
-	toString = strings.Trim(toString, trimSign)
-	_, err := msg.WriteString("To: " + toString + "\r\nFrom: " + mail.From + "<" + mail.From + ">\r\nSubject: " + mail.Subject + "\r\n" + "Content-Type: multipart/mixed; boundary=" + assignBoundary + "\r\n\r\n")
+
+	addressInfo := language.Explode(this.Host, ":")
+	if len(addressInfo) != 2 {
+		return errors.New("invalid host")
+	}
+	host := addressInfo[0]
+	port, err := strconv.Atoi(addressInfo[1])
 	if err != nil {
 		return err
 	}
-
-	//body
-	_, err = msg.WriteString(middleBoundary + "\r\n" + "Content-Type:text/html; charset=UTF-8\r\n\r\n")
-	if err != nil {
-		return err
+	d := &gomail.Dialer{
+		Host:     host,
+		Port:     port,
+		Username: mailAuth.UserName,
+		Password: mailAuth.Password,
+		SSL:      this.SSL,
 	}
-	_, err = msg.WriteString(mail.Body + "\r\n\r\n")
-	if err != nil {
-		return err
-	}
-
-	//attach
-	for _, value := range mail.Attach {
-		_, err = msg.WriteString(middleBoundary + "\r\n" + "Content-Type: application/octet-stream; charset=UTF-8\r\nContent-Disposition: attachment;filename=" + value.Name + "\r\nContent-Description: File Transfer" + "\r\nContent-Transfer-Encoding: base64\r\n\r\n")
-		if err != nil {
-			return err
-		}
-		afterData := this.base64Encode(value.Data)
-		_, err = msg.Write(afterData)
-		if err != nil {
-			return err
-		}
-		_, err = msg.WriteString("\r\n\r\n")
-		if err != nil {
-			return err
-		}
-	}
-
-	//结束标志
-	_, err = msg.WriteString(endBoundary + "\r\n")
-	if err != nil {
-		return err
-	}
-
-	//send message
-	return smtp.SendMail(this.Host, auth, mail.From, mail.To, msg.Bytes())
-}
-
-func (this *SmtpSdk) base64Encode(data []byte) []byte {
-	singleDataLen := len(data)
-	afterDataLen := base64.StdEncoding.EncodedLen(singleDataLen)
-	afterData := make([]byte, afterDataLen)
-	base64.StdEncoding.Encode(afterData, data)
-	return afterData
+	d.TLSConfig = this.TLSConfig
+	return d.DialAndSend(m)
 }
--- src/github.com/fishedee/sdk/smtpsdk_test.go
+++ src/github.com/fishedee/sdk/smtpsdk_test.go
@@ -1,70 +1,75 @@
 package sdk
 
 import (
-//"fmt"
-//"testing"
+	//"fmt"
+	"crypto/tls"
+	"testing"
 )
 
-type MailTestCase struct {
-	host     string
-	from     string
-	password string
-	to       []string
-	subject  string
-	body     string
-}
-
-func sendSingleCase(singleTestCase MailTestCase) error {
-	mailSdk := SmtpSdk{
-		Host: singleTestCase.host,
+func GGTestSmtpSdkNormal(t *testing.T) {
+	smtp := &SmtpSdk{
+		Host: "smtp.163.com:25",
 	}
-	from := singleTestCase.from
-	to := singleTestCase.to
-	subject := singleTestCase.subject
-	body := singleTestCase.body
-	mail := SmtpSdkMail{
-		From:    from,
-		To:      to,
-		Subject: subject,
-		Body:    body,
-	}
-	mailAuth := SmtpSdkMailAuth{
-		UserName: from,
-		Password: singleTestCase.password,
-	}
-	return mailSdk.Send(mailAuth, mail)
-}
-
-/*
-func TestSmtpSdkNormal(t *testing.T) {
-	testCase := []MailTestCase{
-		//不同文本
-		{"smtp.qq.com:25", "jdlau@qq.com", "a19890305272", []string{"jdlau@qq.com"}, "这是测试邮件", "纯文本"},
-		{"smtp.qq.com:25", "jdlau@qq.com", "a19890305272", []string{"jdlau@qq.com"}, "这是测试邮件2", "<a href=\"www.baidu.com\"><p>html结构化文本</p></a>"},
-	}
-
-	for _, singleTestCase := range testCase {
-		err := sendSingleCase(singleTestCase)
-		if err != nil {
-			t.Error("error is not nil" + err.Error())
-		}
+	err := smtp.Send(SmtpSdkMailAuth{
+		UserName: "15018749403@163.com",
+		Password: "password",
+	}, SmtpSdkMail{
+		From:    "15018749403@163.com",
+		To:      []string{"306766045@qq.com"},
+		Subject: "测试标题_normal",
+		Body:    "测试body_noraml<a href=\"http://www.baidu.com\"><p>html结构化文本</p></a>",
+	})
+	if err != nil {
+		panic(err)
 	}
 }
 
-
-func TestSmtpSdkError(t *testing.T) {
-	testCase := []MailTestCase{
-		//something wrong
-		{"smtp.qq.com:25", "jdlau@qq.com", "a19890305272", nil, "这是测试邮件3", "纯文本"},
-		{"smtp.qq.com:25", "jdlau@qq.com", "wrongpass", []string{"jdlau@qq.com"}, "这是测试邮件4", "纯文本"},
+func GGTestSmtpSdkSSL(t *testing.T) {
+	smtp := &SmtpSdk{
+		Host:      "smtp.163.com:465",
+		SSL:       true,
+		TLSConfig: &tls.Config{InsecureSkipVerify: true},
 	}
-
-	for _, singleTestCase := range testCase {
-		fmt.Printf("%#v\n", singleTestCase)
-		err := sendSingleCase(singleTestCase)
-		if err == nil {
-			t.Error("error is not nil" + err.Error())
-		}
+	err := smtp.Send(SmtpSdkMailAuth{
+		UserName: "15018749403@163.com",
+		Password: "password",
+	}, SmtpSdkMail{
+		From:    "15018749403@163.com",
+		To:      []string{"306766045@qq.com"},
+		Subject: "测试标题_ssl",
+		Body:    "测试body_ssl",
+	})
+	if err != nil {
+		panic(err)
 	}
 }
-*/
+
+func GGTestSmtpSdkAttach(t *testing.T) {
+	smtp := &SmtpSdk{
+		Host:      "smtp.163.com:465",
+		SSL:       true,
+		TLSConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	err := smtp.Send(SmtpSdkMailAuth{
+		UserName: "15018749403@163.com",
+		Password: "password",
+	}, SmtpSdkMail{
+		From:    "15018749403@163.com",
+		To:      []string{"306766045@qq.com"},
+		Subject: "测试标题_attach",
+		Body:    "测试body_attach",
+		Attach: []SmtpSdkMailAttach{
+			SmtpSdkMailAttach{
+				Name: "文件1.txt",
+				Data: []byte("文件内容1"),
+			},
+			SmtpSdkMailAttach{
+				Name: "文件2.txt",
+				Data: []byte("文件内容2"),
+			},
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+}
