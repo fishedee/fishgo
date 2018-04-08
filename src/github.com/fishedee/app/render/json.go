e79f6bf72153248c7a4a4de4e6acbc71019d83c6
fishedee <306766045@qq.com>
2018-04-08 20:31:01 +0800

    修复输出json时的content-type

--- src/github.com/fishedee/app/render/json.go
+++ src/github.com/fishedee/app/render/json.go
@@ -13,7 +13,7 @@ func (this *JsonFormatter) Name() string {
 }
 
 func (this *JsonFormatter) Format(w http.ResponseWriter, r *http.Request, data interface{}) error {
-	w.Header().Set("Content-Type", "application/x-javascript; charset=utf-8")
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 
 	dataByte, err := EncodeJson(data)
 	if err != nil {
