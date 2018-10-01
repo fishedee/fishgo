0a26462f70e295ba8a9482022b881297f73b270a
fishedee <306766045@qq.com>
2018-10-01 12:12:37 +0800

    加入微信第三方平台的消息编码与解码

--- src/github.com/fishedee/sdk/wxpluginsdk_test.go
+++ src/github.com/fishedee/sdk/wxpluginsdk_test.go
@@ -1,1 +1,42 @@
 package sdk
+
+import (
+	"encoding/xml"
+	"fmt"
+	. "github.com/fishedee/assert"
+	"testing"
+)
+
+func TestWxPluginSdkEncodingAndDecoding(t *testing.T) {
+	//打包
+	encodingAesKey := "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFG"
+	token := "pamtest"
+	timeStamp := "1409304348"
+	nonce := "xxxxxx"
+	appId := "wxb11529c136998cb6"
+	text := "<xml><ToUserName><![CDATA[oia2Tj我是中文jewbmiOUlr6X-1crbLOvLw]]></ToUserName><FromUserName><![CDATA[gh_7f083739789a]]></FromUserName><CreateTime>1407743423</CreateTime><MsgType><![CDATA[video]]></MsgType><Video><MediaId><![CDATA[eYJ1MbwPRJtOvIEabaxHs7TX2D-HV71s79GUxqdUkjm6Gs2Ed1KF3ulAOA9H1xG0]]></MediaId><Title><![CDATA[testCallBackReplyVideo]]></Title><Description><![CDATA[testCallBackReplyVideo]]></Description></Video></xml>"
+	wxPluginSdk := &WxPluginSdk{
+		ComponentAppId: appId,
+		MessageToken:   token,
+		MessageAESKey:  encodingAesKey,
+	}
+	result, err := wxPluginSdk.encodePackaget(timeStamp, nonce, []byte(text))
+	AssertEqual(t, err, nil)
+
+	//分析
+	var resultObject struct {
+		MsgSignature string `xml:"MsgSignature"`
+		Encrypt      string `xml:"Encrypt"`
+		TimeStamp    string `xml:"TimeStamp"`
+		Nonce        string `xml:"Nonce"`
+	}
+	err = xml.Unmarshal(result, &resultObject)
+	AssertEqual(t, err, nil)
+
+	//解包
+	encryptXml := fmt.Sprintf("<xml><ToUserName><![CDATA[toUser]]></ToUserName><Encrypt><![CDATA[%s]]></Encrypt></xml>", resultObject.Encrypt)
+	toUserName, text2, err := wxPluginSdk.decodePackaget(resultObject.MsgSignature, resultObject.TimeStamp, resultObject.Nonce, []byte(encryptXml))
+	AssertEqual(t, err, nil)
+	AssertEqual(t, toUserName, "toUser")
+	AssertEqual(t, string(text2), text)
+}
