bf1e8686fb98b40056d5298f2b7f9017afd09f5b
fishedee <306766045@qq.com>
2019-03-19 08:59:32 +0800

    加入输出时的防缓存避免stall过长的问题

--- src/github.com/fishedee/app/render/json.go
+++ src/github.com/fishedee/app/render/json.go
@@ -14,7 +14,9 @@ func (this *JsonFormatter) Name() string {
 
 func (this *JsonFormatter) Format(w http.ResponseWriter, r *http.Request, data interface{}) error {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-
+	w.Header().Add("Cache-Control", "private, no-store, no-cache, must-revalidate, max-age=0")
+	w.Header().Add("Cache-Control", "post-check=0, pre-check=0")
+	w.Header().Set("Pragma", "no-cache")
 	dataByte, err := EncodeJson(data)
 	if err != nil {
 		return err
