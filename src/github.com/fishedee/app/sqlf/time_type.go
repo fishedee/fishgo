a45e0827262892c05828191d5ac43e276157ba56
fishedee <306766045@qq.com>
2020-07-03 17:29:16 +0800

    更新sqlite关于时间的处理

--- src/github.com/fishedee/app/sqlf/time_type.go
+++ src/github.com/fishedee/app/sqlf/time_type.go
@@ -8,6 +8,8 @@ import (
 	"time"
 )
 
+//在sqlite3中，并没有时间类型，它其实是个字符串类型。
+//在sqlite3中的驱动中，时间会被Format为"2006-01-02 15:04:05.999999999-07:00"，也就是说，普通的时间格式为2020-07-01 00:00:00+08:00
 func initTimeSqlTypeOperation() {
 	zeroTime := time.Time{}
 	zeroTimeString := zeroTime.Local().Format("2006-01-02T15:04:05")
